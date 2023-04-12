54772e10e9ab34baab8d125dcea8aa28edd281b9
luthfiharw <luthfiharw9@gmail.com>
2023-04-12 15:45:31 +0700

    add treshold

--- Slave/platformio-nano-Slave/src/Sensor.h
+++ Slave/platformio-nano-Slave/src/Sensor.h
@@ -21,6 +21,34 @@ void SensorInit(){
     soil_init();
     // GPS_Init();
 }
+
+void detection(){
+    _rainfall_rate = 5;
+    if (_rainfall_rate > rainfall_rate_danger || _soil_moisture > soil_moisture_danger || _humidity > humidity_danger) {
+        flood_category = 3;
+    } else if ((_humidity > humidity_warning) || (_soil_moisture > soil_moisture_warning)) {
+        flood_category = 2; 
+    } else{
+        flood_category = 1; 
+    }
+    
+    acceleration_magnitude = sqrt(pow(_accel_x, 2) + pow(_accel_y, 2) + pow(_accel_z, 2));
+    // if (acceleration_magnitude > acceleration_danger && _soil_moisture > soil_moisture_warning) {
+    //     landslide_category = 3; 
+    // } else if ((acceleration_magnitude > acceleration_warning && acceleration_magnitude <= acceleration_danger) || (_soil_moisture > soil_moisture_safe && _soil_moisture <= soil_moisture_warning)) {
+    //     landslide_category = 2; 
+    // } else{
+    //     landslide_category = 1; 
+    // }
+    if (abs(_accel_x) > 1500 || abs(_accel_y) > 1500 && _soil_moisture > soil_moisture_danger) {
+        landslide_category = 3; 
+    } else if (abs(_accel_x) > 800 || abs(_accel_y) > 800 || _soil_moisture > soil_moisture_warning) {
+        landslide_category = 2; 
+    } else{
+        landslide_category = 1; 
+    }
+}
+
 // 
 void UpdateSensorData(){
     anemo_update();
@@ -29,6 +57,7 @@ void UpdateSensorData(){
     // get_GPS_Data();
     determineRainFall();
     GetIMUData();
+    detection();
 
 /*JSON*/
     sensor_data["wind"] = _wind_speed;
@@ -41,6 +70,9 @@ void UpdateSensorData(){
     sensor_data["acx"] = AcX;
     sensor_data["acy"] = AcY;
     sensor_data["acz"] = AcZ;
+    sensor_data["flood"] = flood_category;
+    sensor_data["land"] = landslide_category;
+    // sensor_data["x"] = acceleration_magnitude;
     serializeJson(sensor_data, Serial);
     Serial.println();
 
@@ -64,7 +96,6 @@ void UpdateSensorData(){
 // Serial.print(AcZ);
 // Serial.print("#");
 // Serial.println();
-
 }
 
 /*Optional
